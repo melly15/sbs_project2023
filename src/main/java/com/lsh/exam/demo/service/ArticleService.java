8ba8f18c2aa184afa6b64ccbf2db3c88dbff830a
melly15 <melly7015@hanmail.net>
2023-04-18 18:45:34 +0900

    게시물 삭제시 권한 체크

--- src/main/java/com/lsh/exam/demo/service/ArticleService.java
+++ src/main/java/com/lsh/exam/demo/service/ArticleService.java
@@ -11,33 +11,49 @@ import com.lsh.exam.demo.vo.ResultData;
 
 @Service
 public class ArticleService {
-	private ArticleRepository articleRepository;
-	
-	public ArticleService(ArticleRepository articleRepository) {
-		this.articleRepository = articleRepository;
-		
-	}
-	
-	public Article getArticle(int id) {
-		return articleRepository.getArticle(id);
-	}
-	
-	public List<Article> getArticles() {
-		return articleRepository.getArticles();
-	}
-	
-	public ResultData<Integer> writeArticle(int memberId, String title, String body) {
-		articleRepository.writeArticle(memberId, title, body);
-		
-		int id = articleRepository.getLastInsertId();
-		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성 되었습니다.", id), id);
-	}
-	
-	public void deleteArticle(int id) {
-		articleRepository.deleteArticle(id);
-	}
+   private ArticleRepository articleRepository;
+   
+   public ArticleService(ArticleRepository articleRepository) {
+      this.articleRepository = articleRepository;
+      
+   }
+   
+   public Article getArticle(int id) {
+      return articleRepository.getArticle(id);
+   }
+   
+   public List<Article> getArticles() {
+      return articleRepository.getArticles();
+   }
+   
+   public ResultData<Integer> writeArticle(int memberId, String title, String body) {
+      articleRepository.writeArticle(memberId,title, body);
+      
+      int id = articleRepository.getLastInsertId();
+      return ResultData.from("S-1", Ut.f("%d번 게시물이 생성 되었습니다.", id), id);
+   }
+   
+   public void deleteArticle(int id) {
+      articleRepository.deleteArticle(id);
+   }
 
-	public void modifyArticle(int id, String title, String body) {
-		articleRepository.modifyArticle(id, title, body);
-	}
+   public ResultData<Article> modifyArticle(int id, String title, String body) {
+      articleRepository.modifyArticle(id, title, body);
+      
+      Article article = getArticle(id);
+      
+      return ResultData.from("S-1", Ut.f("%d번 게시물이 수정되었습니다.", id), article);
+   }
+
+   public ResultData actorCanModify(int actorId, Article article) {
+      if ( article == null ) {
+         return ResultData.from("F-", "권한이 없습니다.");
+      }
+      
+      if  ( article.getMemberId() != actorId) {
+         return ResultData.from("F-2", "권한이 없습니다.");
+      }
+      
+      return ResultData.from("S-1", "수정 가능합니다.");
+   }
 }
--- src/main/java/com/lsh/exam/demo/vo/ResultData.java
+++ src/main/java/com/lsh/exam/demo/vo/ResultData.java
@@ -11,28 +11,31 @@ public class ResultData<DT> {
 	private String msg;
 	
 	@Getter
+	private String data1Name;
+	
+	@Getter
 	private  DT data1;
 	
 	//생성자 만들어서 객체 생성못하게?
 	private ResultData() {}
 	
 	public static ResultData from(String rsultCode, String msg) {
-		
-		return from(rsultCode, msg , null);
+		return from(rsultCode, msg , null, null);
 	}
 	
-	public static  <DT> ResultData<DT> from (String resultCode, String msg, DT data1) {
+	public static  <DT> ResultData<DT> from (String resultCode, String msg, String data1Name, DT data1) {
 		ResultData<DT> rd = new ResultData<DT>();
 		rd.resultCode = resultCode;
 		rd.msg = msg;
+		rd.data1Name = data1Name;
 		rd.data1 = data1;
 		
 		return rd;
 	}
 	
-	public static<DT> ResultData<DT> newData(ResultData joinRd, DT newData) {
+	public static<DT> ResultData<DT> newData(ResultData oldRd, String data1Name, DT data1) {
 		
-		return from(joinRd.getResultCode(), joinRd.getMsg(), newData);
+		return from(oldRd.getResultCode(), oldRd.getMsg(), data1Name, data1);
 	}
 	
 	public boolean isSuccess() {
