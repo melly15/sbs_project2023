7ee2ca01e0b6a1a562306fe1116ac955e77d1adb
melly15 <melly7015@hanmail.net>
2023-04-20 17:47:35 +0900

    Rq객체를 BeforeActionInterceptor에서 대표로 한번만 받기

--- src/main/java/com/lsh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/lsh/exam/demo/controller/UsrArticleController.java
@@ -26,7 +26,7 @@ public class UsrArticleController {
    @RequestMapping("/usr/article/doAdd")
    @ResponseBody
    public ResultData<Article> doAdd(HttpServletRequest req, String title, String body) {
-      Rq rq = new Rq(req);
+      Rq rq = (Rq)req.getAttribute("rq");
       
       
       if ( rq.isLogined() == false) {
@@ -54,7 +54,7 @@ public class UsrArticleController {
    @RequestMapping("/usr/article/list")
    public String showList(HttpServletRequest req,  Model model) {
 	   
-	  Rq rq = new Rq(req);
+	  Rq rq = (Rq)req.getAttribute("rq");
 	  
       List<Article>  articles = articleService.getForPrintArticles(rq.getLoginedMemberId());
       model.addAttribute("articles", articles);
@@ -64,7 +64,7 @@ public class UsrArticleController {
    
    @RequestMapping("/usr/article/detail")
    public String showDetail(HttpServletRequest req, Model model, int id) {
-	   Rq rq = new Rq(req);
+	   Rq rq = (Rq)req.getAttribute("rq");
 	   
 	   Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 	   model.addAttribute("article", article);
@@ -74,7 +74,7 @@ public class UsrArticleController {
    @RequestMapping("/usr/article/getArticle")
    @ResponseBody
    public ResultData<Article> getArticle(HttpServletRequest req, int id) {
-	  Rq rq = new Rq(req);
+	  Rq rq = (Rq)req.getAttribute("rq");
 	      
 	  
       Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
@@ -90,7 +90,7 @@ public class UsrArticleController {
    @ResponseBody
    public String doDelete(HttpServletRequest req, int id) {
       
-	  Rq rq = new Rq(req);
+	  Rq rq = (Rq)req.getAttribute("rq");
 	   
 
       if ( rq.isLogined() == false) {
@@ -115,7 +115,7 @@ public class UsrArticleController {
    @RequestMapping("/usr/article/doModify")
    @ResponseBody
    public ResultData<Article> doModify(HttpServletRequest req, int id, String title, String body) {
-	  Rq rq = new Rq(req);
+	  Rq rq = (Rq)req.getAttribute("rq");
       
       if ( rq.isLogined() == false) {
          return ResultData.from("F-A","로그인 후 이용 해주세요.");
